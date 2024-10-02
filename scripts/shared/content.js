6d11bf33127b3bd536efe2b03fe693833ecac0a2
Yazeed AlKhalaf <yazeed@alkhalaf.dev>
2024-10-02 13:41:36 +0300

    make the dark mode and enhance stuff all around
    Co-authored-by: Khaled AlAnbar <khaledalanbar@outlook.com>

--- scripts/shared/content.js
+++ scripts/shared/content.js
@@ -1,17 +1,51 @@
-const link1 = document.createElement("link");
-link1.rel = "preconnect";
-link1.href = "https://fonts.googleapis.com";
+setupFonts();
+setupTheme();
+setupLogo();
 
-const link2 = document.createElement("link");
-link2.rel = "preconnect";
-link2.href = "https://fonts.gstatic.com";
-link2.crossOrigin = "anonymous";
+function setupFonts() {
+  const link1 = document.createElement("link");
+  link1.rel = "preconnect";
+  link1.href = "https://fonts.googleapis.com";
 
-const link3 = document.createElement("link");
-link3.rel = "stylesheet";
-link3.href =
-  "https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap";
+  const link2 = document.createElement("link");
+  link2.rel = "preconnect";
+  link2.href = "https://fonts.gstatic.com";
+  link2.crossOrigin = "anonymous";
 
-document.head.appendChild(link1);
-document.head.appendChild(link2);
-document.head.appendChild(link3);
+  const link3 = document.createElement("link");
+  link3.rel = "stylesheet";
+  link3.href =
+    "https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap";
+
+  document.head.appendChild(link1);
+  document.head.appendChild(link2);
+  document.head.appendChild(link3);
+}
+
+function setupTheme() {
+  function setTheme(theme) {
+    document.documentElement.setAttribute("data-theme", theme);
+  }
+
+  setTheme("dark");
+  // setTheme("light"); // Switch to light theme
+}
+
+function setupLogo() {
+  const logoElement = document
+    .querySelector("#container")
+    .querySelector("#all")
+    .querySelector("#top")
+    .querySelector(".logo");
+
+  logoElement.innerHTML = "";
+
+  fetch(chrome.runtime.getURL("assets/yulogo.svg"))
+    .then((response) => response.text())
+    .then((svgText) => {
+      logoElement.innerHTML = svgText;
+
+      // const svgElement = logoElement.querySelector("svg");
+    })
+    .catch((error) => console.error("Error fetching SVG:", error));
+}
